ab639e0b8a2d13569de876425fd28b4a9f29bbc3
Franco Loto <franco.loto1995@gmail.com>
2023-04-01 12:30:21 -0300

    utlimos cambios

--- static/home/js/app.js
+++ static/home/js/app.js
@@ -10,25 +10,5 @@ desplazarArriba.addEventListener("click", () => {
 });
 
 
-// Selecciones para Barra de navegación hamburguesa
 
-const burger = document.getElementById("burger-menu");
-const navegacion = document.querySelector(".navegacion");
-
-
-burger.addEventListener("click", () => {
-  navegacion.classList.toggle("show");
-});
-
-
-// Cerrar el menú de hambuguesa cuando se hace click en un enlace  
-
-// Seleccionar enlaces de navegación
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    navegacion.classList.remove("show");
-})
-);
 
